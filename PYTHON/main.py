3da89f3ddb22a94c74485ccc0f0bbe931efe8f30
trannhat-2004 <tranphinhat25@outlook.com>
2025-09-30 23:38:40 +0700

    commit

--- PYTHON/main.py
+++ PYTHON/main.py
@@ -1,1 +1,20 @@
-print("Hello World ssss")
+#print("Hello one mỏe time")
+#evething in input() is string
+# two number divide always return float
+# take the number as input
+# take the initial cell population as input
+cells = int(input())
+
+# take the number of days as input
+days = int(input())
+
+# initialize the day counter
+counter = 1
+
+#complete the while loop
+while counter <= days:
+  cells = cells * 2
+  # Daily message
+  print("Day " + str(counter) + ": " +str(cells))
+  counter = counter + 1
+  
---
+++ Web development/html/web9.html
@@ -0,0 +1,1 @@
+<button>my button</button>
