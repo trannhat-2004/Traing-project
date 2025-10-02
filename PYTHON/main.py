e11fec4dd4f71ff7fa116a12c8b300a7a3aedc73
trannhat-2004 <tranphinhat25@outlook.com>
2025-10-02 22:18:30 +0700

    2/10/2025

--- PYTHON/main.py
+++ PYTHON/main.py
@@ -3,18 +3,46 @@
 # two number divide always return float
 # take the number as input
 # take the initial cell population as input
-cells = int(input())
+# cells = int(input())
 
-# take the number of days as input
-days = int(input())
+# # take the number of days as input
+# days = int(input())
 
-# initialize the day counter
-counter = 1
+# # initialize the day counter
+# counter = 1
 
-#complete the while loop
-while counter <= days:
-  cells = cells * 2
-  # Daily message
-  print("Day " + str(counter) + ": " +str(cells))
-  counter = counter + 1
-  
+# #complete the while loop
+# while counter <= days:
+#   cells = cells * 2
+#   # Daily message
+#   print("Day " + str(counter) + ": " +str(cells))
+#   counter = counter + 1
+# a = 18 
+# is_student = True
+# if a < 18:
+#     if is_student:
+#         print("You get a discount")
+#     else:
+#         print("You don't get a discount")
+# else:
+#     print("You don't get a discount")
+# b = [] 
+# for i in range(3):
+#     a = int(input('Enter a number:'))
+#     b.append(a)
+
+# my_pet = b[2]
+# print(my_pet)   
+# số đầu tiên mang tính bao gồm, số cuối cùng mang tính loại trừ
+list = ["Tran", "phi",'Nhat']
+print(list[0])
+word = [list[:2]]
+print(word)
+#step counts for the past week
+steps = [1513, 5035, 7891, 1212, 2534, 4648, 3785]
+
+#make a slice for the last 3 days
+last_3 = steps[-3:]
+
+#display the slice
+print(last_3)
